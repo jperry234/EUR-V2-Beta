98d9987f957a62202497db48d4362a6d267c9ae0
jperry234 <joshperry234@gmail.com>
2024-11-18 13:04:40 -0500

    balancing game

--- eopData/eopScripts/eur/eurEOPUnits.lua
+++ eopData/eopScripts/eur/eurEOPUnits.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a867e97e8d94cf6691d6692fbcf7f070138983865eee426fff12e8e80348c67f
-size 152365
+oid sha256:5a4043be6cef9450babcd100850e7e9c3981883ecad43fb3b372736592579a00
+size 152555
