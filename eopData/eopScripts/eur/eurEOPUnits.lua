fc6ae7933d7f65f5eefaecd5aeee275f32aa3a44
jperry234 <joshperry234@gmail.com>
2024-11-20 23:50:22 -0500

    fixing spy and descr

--- eopData/eopScripts/eur/eurEOPUnits.lua
+++ eopData/eopScripts/eur/eurEOPUnits.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a4043be6cef9450babcd100850e7e9c3981883ecad43fb3b372736592579a00
-size 152555
+oid sha256:4db277573b4fdf7aa2bc917bac1931e121de08767527d7c4df34c0187b8c8694
+size 152458
