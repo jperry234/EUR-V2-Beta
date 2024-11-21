dad62d5cd66a7d36051503ac256071eb8da84b2c
jperry234 <joshperry234@gmail.com>
2024-11-21 17:26:23 -0500

    adding dourhand treb and catapult

--- eopData/eopScripts/eur/eurEOPUnits.lua
+++ eopData/eopScripts/eur/eurEOPUnits.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4db277573b4fdf7aa2bc917bac1931e121de08767527d7c4df34c0187b8c8694
-size 152458
+oid sha256:9b79998dfb561768464fdf0b2b64d0a8e1e624156ecd2dccb9baec1fa3855f47
+size 152656
