be1b8401291762aef3aeb2fbe9212255dc1245e6
jperry234 <joshperry234@gmail.com>
2025-01-15 16:38:26 -0500

    many fixes and changes for trolls and mordor

--- eopData/eopScripts/eur/campaign/EurPostBattleLoot.lua
+++ eopData/eopScripts/eur/campaign/EurPostBattleLoot.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebc851156b9324426482b25744fcb03a110b9bf4cf8e6be4fb0a669c013ff726
-size 8621
+oid sha256:810872b9592cb64ebab97a83c83afd8d167bf86c843754acab5c6ad807d7f7e1
+size 8625
--- eopData/eopScripts/eur/units/EurEOPUnits.lua
+++ eopData/eopScripts/eur/units/EurEOPUnits.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdfa2f4348fbcdef0e8d305d3d8d28f44136618ee61a217a10eba5a0c90054a2
-size 155218
+oid sha256:cae81d4ba0394a0b22b7d971a68c33fd96253d170dd8224a0999aeea4908eeac
+size 154565
